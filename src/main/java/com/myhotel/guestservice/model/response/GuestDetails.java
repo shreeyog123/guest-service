417be3fd38c88c7264e3b5a54e9eed64f89d2de9
Shreeyog_Nimkar <shreeyog.nimkar@gmail.com>
2021-01-19 22:32:19 +0530

    added logic and refactor

--- src/main/java/com/myhotel/guestservice/model/response/GuestDetails.java
+++ src/main/java/com/myhotel/guestservice/model/response/GuestDetails.java
@@ -23,10 +23,4 @@ public class GuestDetails {
 
     private PaymentDetails paymentDetails;
 
-
-
-
-
-
-
 }
--- src/main/java/com/myhotel/guestservice/model/response/StayHistory.java
+++ src/main/java/com/myhotel/guestservice/model/response/StayHistory.java
@@ -1,12 +1,21 @@
 package com.myhotel.guestservice.model.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StayHistory {
 
+    private Long hotelId;
+    private String hotelName;
+    private String bookedRoomCode;
     private LocalDate bookingStartDate;
     private LocalDate bookingEndDate;
 }
